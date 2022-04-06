93418e6988498462c013fbea0a12abd83ac58046
nobergan <68248853+Nobergan@users.noreply.github.com>
2022-04-07 00:00:02 +0300

    desk ver

--- src/main/index.js
+++ src/main/index.js
@@ -1,16 +1,2 @@
 import "./scss/index.scss";
 import "./js/swiper-slider";
-
-// import "./js"
-
-// import { CountUp } from 'countup.js';
-
-// const options = {
-//   startVal: 1543327,
-//     duration: 100000,
-//   prefix: '$',
-// }
-// window.onload = function () {
-//     var countUp = new CountUp('count-num', 99000000, options);
-//     countUp.start();
-// }
